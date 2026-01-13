81a228dc8413f000c1d67363bfddee758339b46e
sumeetby16 <sumeetby16@gmail.com>
2026-01-13 19:56:09 +0530

    Update and rename q7.py to employee.py

---
+++ employee.py
@@ -0,0 +1,7 @@
+def get_employee_details(name, emp_id, department, salary):
+    return (
+        f"Employee Name: {name}\n"
+        f"Employee ID: {emp_id}\n"
+        f"Department: {department}\n"
+        f"Salary: {salary}"
+    )
